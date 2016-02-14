8d7f8055b05fb0e91c371353cd7df6a8d06ba235
Eraac <kevin@labesse.me>
2016-02-14 12:24:17 +0100

    customize headers for preflight with parameter.yml

--- src/LKE/CoreBundle/Controller/CorsController.php
+++ src/LKE/CoreBundle/Controller/CorsController.php
@@ -9,7 +9,22 @@ class CorsController extends Controller
 {
     public function preflightAction()
     {
-        // TODO add header instead use apache
-        return new Response();
+        /* Add on your vhost
+         *
+         * RewriteEngine on
+         * RewriteCond %{REQUEST_METHOD} OPTIONS
+         * RewriteCond %{REQUEST_URI} !=/preflight
+         * RewriteRule ^(.*)$ /preflight [END]
+         */
+
+        $origin     = $this->getParameter("allow_origin");
+        $methods    = $this->getParameter("allow_methods");
+        $headers    = $this->getParameter("allow_headers");
+
+        return new Response("", 200, [
+            "Access-Control-Allow-Origin" => $origin,
+            "Access-Control-Allow-Methods" => $methods,
+            "Access-Control-Allow-Headers" => $headers,
+        ]);
     }
 }
