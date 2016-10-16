444677c6d64802a15dabed262655b568d23c5ce7
Eraac <kevin@labesse.me>
2016-10-16 18:48:40 +0200

    add FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new UserBundle\UserBundle(),
+            new CoreBundle\CoreBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
